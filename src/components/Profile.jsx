cd663a9883ea1577d501b9e2d57f5502758df490
Your Name <romaburlaka1995@gmail.com>
2020-11-12 14:00:23 +0200

    logo

---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    
+    <div className='content'>
+    <div>
+      <img src='https://img5.goodfon.ru/wallpaper/big/e/5c/beach-beautiful-beach-ocean-beach-palm-tree-tropical-beach.jpg' />
+    </div>
+    <div>
+      ava + description
+    </div>
+    <div>
+      my post
+      <div>
+        new post
+      </div>
+      <div>
+        post 1
+      </div>
+      <div>
+        post 2
+      </div>
+    </div>
+  </div>
+  );
+}
+
+
+export default Profile;
