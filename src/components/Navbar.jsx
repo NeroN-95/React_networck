31f1befce8a7c7a75e9b47fcda9e09adff2ac7f6
Your Name <romaburlaka1995@gmail.com>
2020-11-12 15:13:39 +0200

    add new files css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
+import './Navbar.css';
 
 const Navbar = () => {
   return (
     
     <div  className='nav'>
-<div>
+<div ClassNamae="item">
   <a>Profile</a>
 </div>
-<div>
+<div  className='nav'>
   <a>Messages</a>
 </div>
-<div>
+<div  className='nav'>
   <a>News</a>
 </div>
-<div>
+<div  className='nav'>
   <a>Music</a>
 </div>
-<div>
+<div  className='nav'>
   <a>Settings</a>
 </div>
     </div>
