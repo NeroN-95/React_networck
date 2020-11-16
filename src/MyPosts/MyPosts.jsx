cb3be6dc98ee51eaa7cd9f594bd0e09db1ca51b8
Your Name <romaburlaka1995@gmail.com>
2020-11-16 07:17:55 +0200

    create component post

--- src/MyPosts/MyPosts.jsx
+++ src/MyPosts/MyPosts.jsx
@@ -1,33 +1,23 @@
 import React from 'react';
 import s from '../MyPosts/MyPosts.module.css';
+import Post from './Post/Post';
 
 const MyPosts = () => {
   return (
-    
+
+    <div>
+      My post
       <div>
-        My post
-      <div>
-          New post
+        <textarea></textarea>
+        <button>Add post</button>
       </div>
-        <div className={s.post} >
-          <div className={s.item}>
-            Post 1
-          </div >
-          <div className='item'>
-            Post 2
-          </div>
-          <div className='item'>
-            Post 3
-          </div >
-          <div className='item'>
-            Post 4
-          </div >
-          <div className='item'>
-            Post 5
-          </div >
-        </div>
+      <div className={s.posts} >
+      <Post />
+      <Post />
+      <Post />
       </div>
-   
+    </div>
+
   );
 }
 
