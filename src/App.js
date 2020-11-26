40e7b57d15ecfb0b4c43de08422dc17b93ecb4f2
Your Name <romaburlaka1995@gmail.com>
2020-11-27 00:21:11 +0200

    create dialogs

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
+import  {Route,BrowserRouter} from "react-router-dom";
 
 
 const App = (props) => {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            <div className="app-wrapper-content">
-                <Dialogs/>
-
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className="app-wrapper-content">
+                    <Route path='/message' component={Dialogs}/>
+                    <Route path='/profile' component={Profile}/>
+                </div>
             </div>
-            {/*<Profile/>*/}
-
-        </div>
+        </BrowserRouter>
     );
 }
 
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,9 +4,30 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog +' ' + s.active}>
+                    Roma
+                </div>
+                <div className={s.dialog}>
+                    Victor
+                </div>
+                <div className={s.dialog}>
+                    Kirill
+                </div>
+                <div className={s.dialog}>
+                    Vlad
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}> hi</div>
+                <div className={s.message}> how is your project</div>
+                <div className={s.message}> good</div>
+            </div>
         </div>
+
+
+
     )
 }
 
