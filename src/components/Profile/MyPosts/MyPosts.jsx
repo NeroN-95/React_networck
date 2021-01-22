b56010cbd93a79266aad90731f0edf6c64228201
NeroN-95 <romaburlaka1995@gmail.com>
2021-01-22 23:16:35 +0200

    action creator\ action type

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
+import {addPostActionCreator, updateNewPostTextActionCreator} from "../../redux/state";
+
+
 
 const MyPosts = (props) => {
     let postsElemets = props.posts.map( p => <Post message={p.message} LikesCount={p.likesCount}/> );
@@ -9,14 +12,14 @@ const MyPosts = (props) => {
 
    let addPost = () => {
 
-       props.dispatch({type:'ADD-POST'});
+       props.dispatch(addPostActionCreator());
 
    }
 
    let onPostChange = () => {
 
        let text = newPostElement.current.value;
-       let action = {type:'UPDATE-NEW-POST-TEXT', NewText : text};
+       let action = updateNewPostTextActionCreator(text);
        props.dispatch(action);
 
    }
--- src/components/redux/state.js
+++ src/components/redux/state.js
@@ -1,4 +1,9 @@
+
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
 let store = {
+
 
     _state: {
         profilePage: {
@@ -57,7 +62,7 @@ let store = {
     },
 
     dispatch(action) {
-        if (action.type === 'ADD-POST') {
+        if (action.type === ADD_POST) {
             let newPost = {
                 id: 5,
                 message: this._state.profilePage.newPostText,
@@ -67,13 +72,15 @@ let store = {
             this._state.profilePage.newPostText = '';
             this._callSubscriber(this._state);
 
-        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+        } else if (action.type === UPDATE_NEW_POST_TEXT) {
             this._state.profilePage.newPostText = action.NewText;
             this._callSubscriber(this._state);
         }
     }
 }
 
+export const addPostActionCreator = () => ({type: 'ADD-POST'})
+export const updateNewPostTextActionCreator = (text) => ({type: 'UPDATE-NEW-POST-TEXT', NewText : text })
 
 export default store;
 window.state = store;
